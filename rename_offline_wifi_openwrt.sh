470de197868546fcd40ab3107afb09f8e7daed89
SashaMa <amalanov@gmail.com>
2022-05-12 00:38:34 +0300

    Create rename_offline_wifi_openwrt.sh
    Removed hardcoded config line names. Now it takes all config lines which end with "ssid" and changes them

--- rename_offline_wifi_openwrt.sh
+++ rename_offline_wifi_openwrt.sh
@@ -1,36 +1,42 @@
-THERE_WERE_ANY_CHAGES=0 # should I commit changes and restart wifi?
-sfx="__OFFLINE" # postfix for ssid
-
-ping -c 3 8.8.8.8
-has_connection=$?
-
-for wifinum in 0 1 2 3 4 5 6 7 8 9
-do
-        ssid=$(uci get wireless.wifinet${wifinum}.ssid)
-        has_sfx=$(echo $ssid | grep -c $sfx)
-
-        if [ $has_connection -eq 0 ]
-        then
-                echo "You have connection to google"
-                if [ $has_sfx -gt 0 ] ; then
-                        echo "SSID contains $sfx in name"
-                        newssid=${ssid/$sfx/}
-                        uci set wireless.wifinet${wifinum}.ssid=$newssid
-                        THERE_WERE_ANY_CHANGES=1
-                        echo "New ssid is $newssid"
-                fi
-        else
-                echo "No internet"
-                if [ $has_sfx -eq 0 ]; then
-                        newssid="$ssid$sfx"
-                        echo "New ssid is $newssid"
-                        uci set wireless.wifinet${wifinum}.ssid=$newssid
-                        THERE_WERE_ANY_CHANGES=1
-                fi
-        fi
-done
-
-if [ $THERE_WERE_ANY_CHANGES -eq 1 ]; then
-        uci commit wireless
-        wifi reload
-fi
+THERE_WERE_ANY_CHANGES=0                                                                   
+sfx="__OFFLINE"                                                                            
+                                                                                           
+ping -c 3 8.8.8.8                                                                          
+has_connection=$?                                                                          
+                                                                                           
+# get all config lines which contain *ssid=* and iterate over them                         
+uci show | grep "ssid[ ]*=" | while read -r line; do                                       
+        # split config line which looks like devicename=wifiname and get device name       
+        device="$(echo $line | cut -d"=" -f1)"                                             
+        ssid=$(uci get ${device})                                                          
+        has_sfx=$(echo $ssid | grep -c $sfx)                                               
+                                                                                           
+        if [ $has_connection -eq 0 ]                                                       
+        then                                                                               
+                echo "You have connection to google"                                       
+                if [ $has_sfx -gt 0 ] ; then                                               
+                        echo "Gonna fix ' $device' and SSID '$ssid'"                       
+                        echo "SSID contains $sfx in name"                                  
+                        newssid=${ssid/$sfx/}                                              
+                        uci set ${device}=$newssid                                         
+                        THERE_WERE_ANY_CHANGES=1                                           
+                        echo "New ssid is $newssid"                                        
+                fi                                                                         
+        else                                                                               
+                echo "No internet"                                                         
+                if [ $has_sfx -eq 0 ]; then                                                
+                        echo "Gonna fix ' $device' and SSID '$ssid'"                       
+                        newssid="$ssid$sfx"                                                
+                        echo "New ssid is $newssid"                                        
+                        uci set ${device}=$newssid                                         
+                        THERE_WERE_ANY_CHANGES=1                                           
+                fi                                                                         
+        fi                                                                                 
+done                                                                                       
+                                                                                           
+echo "Where there any changes? Is there anything to commit? $THERE_WERE_ANY_CHANGES"       
+if [ $THERE_WERE_ANY_CHANGES -eq 1 ]; then                                                 
+        echo "Commit changes"                                                              
+        uci commit wireless                                                                
+        wifi reload                                                                        
+fi                                             
